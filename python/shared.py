79a2e54ce718e666302ba277fc20535ef167b921
andiwand <stefl.andreas@gmail.com>
2017-02-27 12:28:11 +0100

    refix utc

--- python/shared.py
+++ python/shared.py
@@ -17,14 +17,14 @@ else:
     time = pytime.time
 
 def utc_nano():
-    #utc = pytime.mktime(datetime.datetime.utcnow().timetuple())
+    utc = pytime.mktime(datetime.datetime.utcnow().timetuple())
     t = time()
-    #if int(utc % 100) - int(t % 100) > 50:
-    #    t = int(utc / 100) * 100 + t % 100 + 100
-    #elif int(t % 100) > int(utc % 100):
-    #    t = int(utc / 100) * 100 + t % 100
-    #else:
-    #    t = utc + t - int(t)
+    if int(utc % 100) - int(t % 100) > 50:
+        t = int(utc / 100) * 100 + t % 100 + 100
+    elif int(t % 100) > int(utc % 100):
+        t = int(utc / 100) * 100 + t % 100
+    else:
+        t = utc + t - int(t)
     return t
 
 def ping(dest, timeout=5):
