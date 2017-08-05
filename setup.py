d2585b596ad6e815d999cceded317866c390c1ec
andiwand <stefl.andreas@gmail.com>
2017-08-05 14:39:07 +0200

    rename

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from setuptools import setup
 setup(
     name="time-tests",
     version="0.0.1",
-    url="https://github.com/andiwand/time-tests",
+    url="https://github.com/andiwand/time-experiments",
     license="GNU Lesser General Public License",
     author="Andreas Stefl",
     install_requires=["wiringpi"],
     author_email="stefl.andreas@gmail.com",
-    description="time test scripts",
+    description="time experiment scripts",
     long_description="",
     package_dir={"": "src"},
-    packages=["timetests", "timetests.delay", "timetests.tvt"],
+    packages=["timeexperiments", "timeexperiments.delay", "timeexperiments.tvt"],
     platforms=["linux"],
     entry_points={
         "console_scripts": [
-            "delay-icmp = timetests.delay.icmp:main",
-            "delay-gpio = timetests.delay.gpio:main",
-            "delay-nmea = timetests.delay.nmea:main",
-            "delay-system = timetests.delay.system:main",
-            "tvt-udp-client = timetests.tvt.udp_client:main",
-            "tvt-udp-server = timetests.tvt.udp_server:main",
-            "tvt-gpio-client = timetests.tvt.gpio_client:main",
-            "tvt-gpio-server = timetests.tvt.gpio_server:main"
+            "delay-icmp = timeexperiments.delay.icmp:main",
+            "delay-gpio = timeexperiments.delay.gpio:main",
+            "delay-nmea = timeexperiments.delay.nmea:main",
+            "delay-system = timeexperiments.delay.system:main",
+            "tvt-udp-client = timeexperiments.tvt.udp_client:main",
+            "tvt-udp-server = timeexperiments.tvt.udp_server:main",
+            "tvt-gpio-client = timeexperiments.tvt.gpio_client:main",
+            "tvt-gpio-server = timeexperiments.tvt.gpio_server:main"
         ]
     },
 )
